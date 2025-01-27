b0714e2284775939f899e66dc57761f9e542bf18
Shah Ismail <302743+sim4life@users.noreply.github.com>
2025-01-27 13:58:22 +0300

    Added more dynamic generation

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: 'Astro Learner Sim | Blog',
+    description: 'Sim\'s journey learning Astro 5',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-gb</language>`,
+  });
+}
